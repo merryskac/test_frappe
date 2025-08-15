421afdd90d5071415467c369f1d69861b488cf9c
mewisz <merryskachr@gmail.com>
2025-08-15 10:58:35 +0800

    first commit

---
+++ myapp/myapp/api/project.py
@@ -0,0 +1,62 @@
+import frappe
+from myapp.myapp.utils.apiResponse import api_response
+
+@frappe.whitelist(allow_guest=True)
+def get_project(project_id):
+   project = frappe.get_list("Project", filters={"name": project_id}, fields=["name","name1"])
+   tasks = frappe.get_all("Tasks", filters={"project": project_id}, fields=["name", "parent_task", "status"])
+
+   if len(project)==0:
+      return api_response(status=404, message="Project not found")
+
+   for task in tasks:
+      task["total_child"]= len([d for d in tasks if d.get("parent_task")==task.get("name")])
+
+   print("haizzz")
+   return api_response(status=200, message="Data found", data={
+      "project":project[0],
+      "tasks":tasks
+   } )
+
+@frappe.whitelist(allow_guest=True)
+def list_projects(status="all", page=1, page_size=10):
+   try:
+      page = int(page)
+      page_size = int(page_size)
+   except e:
+      return api_response(status=400, message="Invalid page or page size: should be integers")
+   
+   user_id = "Administrator"
+
+   offset = (page -1)*page_size
+
+   filters = {"owner": user_id}
+
+
+   print(status, page, page_size)
+   if status != "all":
+      filters["status"]= status
+   
+   projects = frappe.get_all(
+      "Tasks", 
+      filters=filters, 
+      fields=["name as id", "name1 as name", "status"],
+      limit_start=offset,
+      limit_page_length=page_size,
+   )
+
+   total_data = frappe.db.count("Tasks", filters=filters)
+
+   return api_response(
+      status=200,
+      message="Data found",
+      data={
+         "data": projects,
+         "total_data": total_data,
+         "page": page,
+         "per_page": page_size,
+         "next_page": page + 1 if offset + page_size < total_data else None,
+         "prev_page": page - 1 if page > 1 else None
+      }
+   )
+
---
+++ myapp/myapp/doctype/tasks/tasks.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, mew and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Tasks(Document):
+	pass
